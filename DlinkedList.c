6e87b033782b4eed8676602802074b87f6a8110b
gulshan <mgulshan2003@gmail.com>
2025-01-20 20:25:24 +0530

    +welcomeScreen(), +screenCleaner().

--- DlinkedList.c
+++ DlinkedList.c
@@ -15,3 +15,51 @@ struct node {
 ++accessibility across program. */
 struct node *headN = NULL;
 int nodeCounter = 1;
+
+void welcomeScreen();       // Introduction to main page & choice screen.
+void screenCleaner();       // Clear the console & input buffer.
+
+// Driver code.
+void main () {
+    welcomeScreen();
+}
+
+// Welcomescreen.
+void welcomeScreen() {
+    int choice;
+
+label1:
+    printf("-- Impelementing & demonstrating operations on Doubly Linked List.\n\n");
+
+    printf("*Read the options carefully and choose.\n");
+    printf("> 1. Insertion.\n");
+    printf("> 2. Deletion.\n");
+    printf("> 3. Update.\n");
+    printf("> 4. Search.\n");
+    printf("> 5. Sort.\n");
+    printf("> 6. Reverse.\n");
+    printf("> 7. Exit the program.\n\n");
+
+    printf("*NOTE: Before choosing options like deletion, search,\n"
+           "reverse,. must insert enough elements.\n\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1:
+            insertion();
+            break;
+        default:
+            printf("Error! choose from the given options.\n");
+            printf("Press any key to exit...");
+            getch();
+            goto label1;
+    }
+}
+
+// Screen cleaner function.
+void screenCleaner() {
+    system("cls");
+    fflush(stdin);
+}
