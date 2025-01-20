6e2a8a490c6c183b18b3b79939f4db25c31bd689
gulshan <mgulshan2003@gmail.com>
2025-01-20 20:00:39 +0530

    +header files, +program variables.

--- DlinkedList.c
+++ DlinkedList.c
@@ -1,1 +1,17 @@
 // Implementing Doubly Linked List - abstract data type.
+
+#include <stdio.h>      // for: input & output.
+#include <conio.h>      // for: getch();
+#include <stdlib.h>     // for: exit(), system();
+
+// Structure that contains a data variable & two pointers variables of it's own type. (NODE)
+struct node {
+    struct node *linkP;
+    int data;
+    struct node *linkN;
+};
+
+/* head node (short for headN) globally declared to reduce the number of parametes, 
+++accessibility across program. */
+struct node *headN = NULL;
+int nodeCounter = 1;
